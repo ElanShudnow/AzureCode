4aa06644e342e2c84cc5c4951b0ba791931f4926
Elan Shudnow <elanshudnow@microsoft.com>
2024-06-27 10:46:50 -0500

    Updated VMRegionalZonalAvailability PowerShell README

---
+++ PowerShell/VMRegionZonalAvailability/Get-VMRegionZonalAvailability.ps1
@@ -0,0 +1,81 @@
+# Replace $Region Array with the following if you want to check all regions: $Regions = (Get-AzLocation).Location
+$Regions = @(
+    'NorthCentralUS',
+    'SouthCentralUS'
+)
+
+# Do not modify below this line
+function Get-SHDOpenFileDialog {
+    [cmdletbinding()]
+    param (
+        [string]$InitialDirectory = "$Env:USERPROFILE",
+        [string]$Title = "Please Select A file",
+        [string]$Filter = "All files (*.*)| *.*",
+        [switch]$MultiSelect
+    )
+    Add-Type -AssemblyName System.Windows.Forms
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog 
+    $FileBrowser.InitialDirectory = "$InitialDirectory"
+    $FileBrowser.Filter = "$Filter"
+    $FileBrowser.Title = "$Title"
+    if ($MultiSelect) {
+        $FileBrowser.Multiselect = $true
+    } else {
+        $FileBrowser.Multiselect = $false
+    }
+ 
+    $FileBrowser.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true })) | Out-Null
+    $FileBrowser.Filenames
+    $FileBrowser.dispose()
+}
+
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptFolder = Split-Path $scriptPath -Parent
+$ExportFile = $scriptFolder + '\' + 'output.csv'
+
+$filename = Get-SHDOpenFileDialog -Title "Select the CSV file" -InitialDirectory $scriptFolder -Filter "CSV Files (*.csv)| *.csv"
+$VMSKUsCSV = Import-Csv $filename
+
+$exportData = @()
+
+foreach ($Region in $Regions)
+{
+    Write-Host "`nChecking for VM SKU Availability in $Region"
+    $RegionData = Get-AzComputeResourceSKU -Location $Region | Where-Object {$_.ResourceType -eq 'VirtualMachines' -and $_.Restrictions.ReasonCode -ne 'NotAvailableForSubscription'}
+
+    foreach ($VMSku in $VMSKUsCSV)
+    {
+        $exportObj = New-Object PSObject
+        if ($RegionData.Name -contains $VMSku.Name)
+        {
+            $Zones = $($RegionData | Where-Object {$_.Name -eq $VMSKU.Name}).LocationInfo.Zones
+            $SortedZones = $Zones -join ","
+            if ($Zones -ne $null)
+            {
+                Write-Host "$($VMSku.Name) Available in $Region in the following Zones: $SortedZones" -ForegroundColor Green
+            }
+            else 
+            {
+                Write-Host "$($VMSku.Name) Available in $Region with no Zonal support." -ForegroundColor Green
+            }
+                
+            $exportObj | Add-Member NoteProperty -Name "Region" -Value $Region
+            $exportObj | Add-Member NoteProperty -Name "SKU" -Value $VMSku.Name
+            $exportObj | Add-Member NoteProperty -Name "Available" -Value 'Yes'
+            $exportObj | Add-Member NoteProperty -Name "Zones" -Value $SortedZones
+
+            $Zones = $null
+            $SortedZones = $null
+        }
+        else 
+        {
+            Write-Host "$($VMSku.Name) Not available in $Region" -ForegroundColor Red
+            $exportObj | Add-Member NoteProperty -Name "Region" -Value $Region
+            $exportObj | Add-Member NoteProperty -Name "SKU" -Value $VMSku.Name
+            $exportObj | Add-Member NoteProperty -Name "Available" -Value 'No'
+        }
+        $exportData = $exportData += $exportObj
+    }
+}
+$exportData | Export-Csv -Path $ExportFile -NoTypeInformation
+Write-Host "`nOutput CSV generated at the following location: $ExportFile" -ForegroundColor Yellow
---
+++ PowerShell/VMRegionZonalAvailability/README.md
@@ -0,0 +1,43 @@
+# Get-VMRegionZonalAvailability.ps1
+## Description
+This PowerShell Script takes a list of Virtual Machine SKUs from a CSV and Regions specified as an array in script and gets a list of what Virtual Machine SKUs are supported in the specified Regions as well as what Availability Zones these VM SKUs are available in.  This script will provide output of the results in both the PowerShell Console as well as a CSV output in the same directory the script is executed from. 
+
+## PowerShell Versions Tested
+- Windows PowerShell 5.1
+- PowerShell 7.2.1
+
+## Files Involved
+- Get-VMRegionAvailability.ps1
+- VMSKUs.csv
+- output.csv (created after script execution)
+
+## Instructions
+1. Download Get-VMRegionAvailability.ps1 and VMSKUs.csv
+   
+2. Edit VMSKUs.csv to include the Virtual Machine SKUs you want to test.
+   
+    ![Alt text](./DemoScreenshots/demo1.jpg?raw=true)
+
+3. Edit Get-VMRegionAvailability.ps1 to include the Regions you want to test Virtual Machine SKU availability against.
+   
+   ![Alt text](./DemoScreenshots/demo2.jpg?raw=true)
+   
+4. Open PowerShell, navigate to script directory, and connect to Azure leveraging Connect-AZAccount
+
+    ![Alt text](./DemoScreenshots/demo3.jpg?raw=true)
+
+5. If wanting to select a different subscription, run Get-AZSubscription and then Select-AZSubscription to change Subscription.
+
+    ![Alt text](./DemoScreenshots/demo4.jpg?raw=true)
+
+6. Execute the script using .\Get-VMRegionAvailability.ps1.  A Windows File Dialog filtering on CSV appears in the same folder the script was executed from.  Choose the CSV containing the list of Virtual Machine SKUs.
+
+    ![Alt text](./DemoScreenshots/demo5.jpg?raw=true)
+
+7. The results are provided in the PowerShell Console. 
+
+    ![Alt text](./DemoScreenshots/demo6.jpg?raw=true)
+
+8. An output CSV is also generated and stored in the same folder the script was executed from. The location is specified at the bottom of the PowerShell Console. 
+   
+   ![Alt text](./DemoScreenshots/demo7.jpg?raw=true)
