eb5cb13bba64d516d1be2c6d2ee65680022c6cbe
Elan Shudnow <elanshudnow@microsoft.com>
2024-06-29 07:27:50 -0500

    Updated VMRegionalZonalAvailability to include information about Logical to Physical Availability Zone Mapping.

--- PowerShell/VMRegionZonalAvailability/Get-VMRegionZonalAvailability.ps1
+++ PowerShell/VMRegionZonalAvailability/Get-VMRegionZonalAvailability.ps1
@@ -1,3 +1,19 @@
+<#
+.SYNOPSIS
+    This PowerShell Script takes a list of Virtual Machine SKUs from a CSV and Regions specified as an array in script and gets a list of what Virtual Machine SKUs are supported in the specified Regions as well as what Availability Zones these VM SKUs are available in. This script will provide output of the results in both the PowerShell Console as well as a CSV output in the same directory the script is executed from.
+
+.EXAMPLE
+    PS C:\> .\Get-VMRegionZonalAvailability
+
+.NOTES
+    AUTHOR: ELAN SHUDNOW - PRINCIPAL CLOUD SOLUTION ARCHITECT | Azure Infrastructure | Microsoft
+    PERMISSIONS: Minimum Permissions Required are Reader
+
+.LINK
+    https://github.com/ElanShudnow/AzureCode/tree/main/PowerShell/VMRegionZonalAvailability
+    Please note that while being developed by a Microsoft employee, Get-VMRegionZonalAvailability is not a Microsoft service or product. Get-VMRegionZonalAvailability is a personal/community driven project, there are none implicit or explicit obligations related to this project, it is provided 'as is' with no warranties and confer no rights.
+#>
+
 # Replace $Region Array with the following if you want to check all regions: $Regions = (Get-AzLocation).Location
 $Regions = @(
     'NorthCentralUS',
@@ -20,7 +36,8 @@ function Get-SHDOpenFileDialog {
     $FileBrowser.Title = "$Title"
     if ($MultiSelect) {
         $FileBrowser.Multiselect = $true
-    } else {
+    }
+    else {
         $FileBrowser.Multiselect = $false
     }
  
@@ -38,44 +55,151 @@ $VMSKUsCSV = Import-Csv $filename
 
 $exportData = @()
 
-foreach ($Region in $Regions)
-{
-    Write-Host "`nChecking for VM SKU Availability in $Region"
-    $RegionData = Get-AzComputeResourceSKU -Location $Region | Where-Object {$_.ResourceType -eq 'VirtualMachines' -and $_.Restrictions.ReasonCode -ne 'NotAvailableForSubscription'}
+$AzContext = Get-AzContext
+$ExistingSubscriptionID = $AzContext.Subscription.ID
+$ExistingSubscriptionName = $AzContext.Subscription.Name
 
-    foreach ($VMSku in $VMSKUsCSV)
+Write-Host " "
+Write-Host "Processing Script in the current subscription: $ExistingSubscriptionID ($ExistingSubscriptionName)" -ForegroundColor Yellow
+
+Write-Host "`nAttempting to contact List Locations API for Logical to Physical Availability Zone Mapping..." -ForegroundColor Yellow
+$AvailablityZoneMapping = Invoke-AzRestMethod -Path "/subscriptions/$ExistingSubscriptionID/locations?api-version=2022-12-01" -Method GET
+$AvailablityZoneMappingContent = $AvailablityZoneMapping.Content
+$AvailablityZoneMappingStatusCode = $AvailablityZoneMapping.StatusCode
+
+if ($AvailablityZoneMappingStatusCode -eq 200) {
+    Write-Host "Successful lookup. Will include Logical to Physical Availability Zone Mapping in data output." -ForegroundColor Green
+    $AvailablityZoneMappingConvertedContent = ($AvailablityZoneMappingContent | ConvertFrom-Json).value
+    $availabilityZoneMappings = $AvailablityZoneMappingConvertedContentRegionMatch.availabilityZoneMappings
+
+    foreach ($Region in $Regions) 
     {
-        $exportObj = New-Object PSObject
-        if ($RegionData.Name -contains $VMSku.Name)
-        {
-            $Zones = $($RegionData | Where-Object {$_.Name -eq $VMSKU.Name}).LocationInfo.Zones
-            $SortedZones = $Zones -join ","
-            if ($Zones -ne $null)
-            {
-                Write-Host "$($VMSku.Name) Available in $Region in the following Zones: $SortedZones" -ForegroundColor Green
-            }
-            else 
-            {
-                Write-Host "$($VMSku.Name) Available in $Region with no Zonal support." -ForegroundColor Green
-            }
-                
-            $exportObj | Add-Member NoteProperty -Name "Region" -Value $Region
-            $exportObj | Add-Member NoteProperty -Name "SKU" -Value $VMSku.Name
-            $exportObj | Add-Member NoteProperty -Name "Available" -Value 'Yes'
-            $exportObj | Add-Member NoteProperty -Name "Zones" -Value $SortedZones
+        Write-Host "`nChecking for VM SKU Availability in $Region"
+        $AvailablityZoneMappingConvertedContentRegionMatch = $AvailablityZoneMappingConvertedContent | Where-Object { $_.Name -eq $Region }
+        $availabilityZoneMappings = $AvailablityZoneMappingConvertedContentRegionMatch.availabilityZoneMappings
+        $RegionalDisplayName = $AvailablityZoneMappingConvertedContentRegionMatch.RegionalDisplayName
+        $RegionData = Get-AzComputeResourceSKU -Location $Region | Where-Object { $_.ResourceType -eq 'VirtualMachines' -and $_.Restrictions.ReasonCode -ne 'NotAvailableForSubscription' }
+  
+        foreach ($VMSku in $VMSKUsCSV) {
+            if ($RegionData.Name -contains $VMSku.Name) {
+                $LogicalZones = $($RegionData | Where-Object { $_.Name -eq $VMSKU.Name }).LocationInfo.Zones
+                $SortedLogicalZones = $LogicalZones -join ","
+                if ($LogicalZones -ne $null) {
+                    foreach ($LogicalZone in $LogicalZones) {
+                        $Zones = $null
+                        $SortedZones = $null
 
-            $Zones = $null
-            $SortedZones = $null
+                        $PhysicalZone = ($availabilityZoneMappings | Where-Object { $_.logicalZone -eq $LogicalZone }).physicalZone[-1]
+                        $PhysicalZones += $PhysicalZone
+                        $exportObj = New-Object PSObject
+                        $exportObj | Add-Member NoteProperty -Name "Region" -Value $Region
+                        $exportObj | Add-Member NoteProperty -name "RegionDisplayName" -Value $RegionalDisplayName
+                        $exportObj | Add-Member NoteProperty -Name "SKU" -Value $VMSku.Name
+                        $exportObj | Add-Member NoteProperty -Name "Available" -Value 'Yes'
+                        $exportObj | Add-Member NoteProperty -Name "PhysicalZone" -Value $PhysicalZone
+                        $exportObj | Add-Member NoteProperty -Name "LogicalZone" -Value $LogicalZone
+                        $exportData = $exportData += $exportObj
+
+                        $Zones = $null
+                        $SortedZones = $null
+                    }
+                    $SortedPhysicalZones = $PhysicalZones -replace '.(?!$)', '$0,'
+                    Write-Host "$($VMSku.Name) Available in $Region in the following Logical Zones: $SortedLogicalZones and Physical Zones: $SortedPhysicalZones" -ForegroundColor Green
+                    $PhysicalZones = $null
+                    $SortedPhysicalZones = $null
+                }
+                else {
+                    $Zones = $null
+                    $SortedZones = $null
+                    Write-Host "$($VMSku.Name) Available in $Region with no Zonal support." -ForegroundColor Green
+                    $exportObj = New-Object PSObject
+                    $exportObj | Add-Member NoteProperty -Name "Region" -Value $Region
+                    $exportObj | Add-Member NoteProperty -name "RegionDisplayName" -Value $RegionalDisplayName
+                    $exportObj | Add-Member NoteProperty -Name "SKU" -Value $VMSku.Name
+                    $exportObj | Add-Member NoteProperty -Name "Available" -Value 'Yes'
+                    $exportObj | Add-Member NoteProperty -Name "PhysicalZone" -Value $PhysicalZone
+                    $exportObj | Add-Member NoteProperty -Name "LogicalZone" -Value $LogicalZone
+                    $exportData = $exportData += $exportObj
+                    $Zones = $null
+                    $SortedZones = $null
+                }
+                  
+            }
+            else {
+                Write-Host "$($VMSku.Name) Not available in $Region" -ForegroundColor Red
+                $exportObj = New-Object PSObject
+                $exportObj | Add-Member NoteProperty -Name "Region" -Value $Region
+                $exportObj | Add-Member NoteProperty -name "RegionDisplayName" -Value $RegionalDisplayName
+                $exportObj | Add-Member NoteProperty -Name "SKU" -Value $VMSku.Name
+                $exportObj | Add-Member NoteProperty -Name "Available" -Value 'No'
+                $exportObj | Add-Member NoteProperty -Name "PhysicalZone" -Value ''
+                $exportObj | Add-Member NoteProperty -Name "LogicalZone" -Value ''
+                $exportData = $exportData += $exportObj
+            }
+         
         }
-        else 
-        {
-            Write-Host "$($VMSku.Name) Not available in $Region" -ForegroundColor Red
-            $exportObj | Add-Member NoteProperty -Name "Region" -Value $Region
-            $exportObj | Add-Member NoteProperty -Name "SKU" -Value $VMSku.Name
-            $exportObj | Add-Member NoteProperty -Name "Available" -Value 'No'
-        }
-        $exportData = $exportData += $exportObj
     }
 }
+else 
+{
+    Write-Host "Unsuccessful lookup. Will only include Logical Zone information in data output." -ForegroundColor Red  
+    foreach ($Region in $Regions) 
+    {
+        Write-Host "`nChecking for VM SKU Availability in $Region"
+        $RegionData = Get-AzComputeResourceSKU -Location $Region | Where-Object { $_.ResourceType -eq 'VirtualMachines' -and $_.Restrictions.ReasonCode -ne 'NotAvailableForSubscription' }
+  
+        foreach ($VMSku in $VMSKUsCSV) {
+            if ($RegionData.Name -contains $VMSku.Name) {
+                $LogicalZones = $($RegionData | Where-Object { $_.Name -eq $VMSKU.Name }).LocationInfo.Zones
+                $SortedLogicalZones = $LogicalZones -join ","
+                if ($LogicalZones -ne $null) {
+                    foreach ($LogicalZone in $LogicalZones) {
+                        $Zones = $null
+                        $SortedZones = $null
+
+                        $exportObj = New-Object PSObject
+                        $exportObj | Add-Member NoteProperty -Name "Region" -Value $Region
+                        $exportObj | Add-Member NoteProperty -Name "SKU" -Value $VMSku.Name
+                        $exportObj | Add-Member NoteProperty -Name "Available" -Value 'Yes'
+                        $exportObj | Add-Member NoteProperty -Name "LogicalZone" -Value $LogicalZone
+                        $exportData = $exportData += $exportObj
+
+                        $Zones = $null
+                        $SortedZones = $null
+                    }
+                    $SortedPhysicalZones = $PhysicalZones -replace '.(?!$)', '$0,'
+                    Write-Host "$($VMSku.Name) Available in $Region in the following Logical Zones: $SortedLogicalZones" -ForegroundColor Green
+                    $PhysicalZones = $null
+                    $SortedPhysicalZones = $null
+                }
+                else {
+                    $Zones = $null
+                    $SortedZones = $null
+                    Write-Host "$($VMSku.Name) Available in $Region with no Zonal support." -ForegroundColor Green
+                    $exportObj = New-Object PSObject
+                    $exportObj | Add-Member NoteProperty -Name "Region" -Value $Region
+                    $exportObj | Add-Member NoteProperty -Name "SKU" -Value $VMSku.Name
+                    $exportObj | Add-Member NoteProperty -Name "Available" -Value 'Yes'
+                    $exportObj | Add-Member NoteProperty -Name "LogicalZone" -Value $LogicalZone
+                    $exportData = $exportData += $exportObj
+                    $Zones = $null
+                    $SortedZones = $null
+                }
+                  
+            }
+            else {
+                Write-Host "$($VMSku.Name) Not available in $Region" -ForegroundColor Red
+                $exportObj = New-Object PSObject
+                $exportObj | Add-Member NoteProperty -Name "Region" -Value $Region
+                $exportObj | Add-Member NoteProperty -Name "SKU" -Value $VMSku.Name
+                $exportObj | Add-Member NoteProperty -Name "Available" -Value 'No'
+                $exportObj | Add-Member NoteProperty -Name "LogicalZone" -Value ''
+                $exportData = $exportData += $exportObj
+            }
+         
+        }
+    } <# Action when all if and elseif conditions are false #>
+}
+
 $exportData | Export-Csv -Path $ExportFile -NoTypeInformation
 Write-Host "`nOutput CSV generated at the following location: $ExportFile" -ForegroundColor Yellow
--- PowerShell/VMRegionZonalAvailability/README.md
+++ PowerShell/VMRegionZonalAvailability/README.md
@@ -1,6 +1,6 @@
 # Get-VMRegionZonalAvailability.ps1
 ## Description
-This PowerShell Script takes a list of Virtual Machine SKUs from a CSV and Regions specified as an array in script and gets a list of what Virtual Machine SKUs are supported in the specified Regions as well as what Availability Zones these VM SKUs are available in.  This script will provide output of the results in both the PowerShell Console as well as a CSV output in the same directory the script is executed from. 
+This PowerShell Script takes a list of Virtual Machine SKUs from a CSV and Regions specified as an array in script and gets a list of what Virtual Machine SKUs are supported in the specified Regions as well as what Availability Zones these VM SKUs are available in.  The script will leverage the List Locations API to also output the Logical to Physical Zone Mapping.  This script will provide output of the results in both the PowerShell Console as well as a CSV output in the same directory the script is executed from. 
 
 ## PowerShell Versions Tested
 - Windows PowerShell 5.1
@@ -18,7 +18,7 @@ This PowerShell Script takes a list of Virtual Machine SKUs from a CSV and Regio
    
     ![Alt text](./DemoScreenshots/demo1.jpg?raw=true)
 
-3. Edit Get-VMRegionAvailability.ps1 to include the Regions you want to test Virtual Machine SKU availability against.
+3. Edit Get-VMRegionZonalAvailability.ps1 to include the Regions you want to test Virtual Machine SKU availability against.
    
    ![Alt text](./DemoScreenshots/demo2.jpg?raw=true)
    
@@ -30,14 +30,26 @@ This PowerShell Script takes a list of Virtual Machine SKUs from a CSV and Regio
 
     ![Alt text](./DemoScreenshots/demo4.jpg?raw=true)
 
-6. Execute the script using .\Get-VMRegionAvailability.ps1.  A Windows File Dialog filtering on CSV appears in the same folder the script was executed from.  Choose the CSV containing the list of Virtual Machine SKUs.
+6. Execute the script using .\Get-VMRegionZonalAvailability.ps1.  A Windows File Dialog filtering on CSV appears in the same folder the script was executed from.  Choose the CSV containing the list of Virtual Machine SKUs.
 
     ![Alt text](./DemoScreenshots/demo5.jpg?raw=true)
 
 7. The results are provided in the PowerShell Console. 
 
+    If a successful List Locations API lookup was performed, the PowerShell output will contain information about the SKU's availability in both Logical Availability Zones and Physical Availability Zones.
+
     ![Alt text](./DemoScreenshots/demo6.jpg?raw=true)
 
+    If an unsuccessful List Locations API lookup was performed, the output will only contain information about the Logical Availability Zones.
+
+    ![Alt text](./DemoScreenshots/demo8.jpg?raw=true)
+
 8. An output CSV is also generated and stored in the same folder the script was executed from. The location is specified at the bottom of the PowerShell Console. 
+
+    If a successful List Locations API lookup was performed, the output will contain information about the Logical Availability Zone to Physical Availability Zone Mapping in greater detail than the PowerShell Output
    
    ![Alt text](./DemoScreenshots/demo7.jpg?raw=true)
+
+    If an unsuccessful List Locations API lookup was performed, the output will contain information only about the Logical Availability Zones.
+   
+   ![Alt text](./DemoScreenshots/demo9.jpg?raw=true)
